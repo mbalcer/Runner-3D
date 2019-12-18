e3098bb50a88546f2473634a72fe97d49ffc8409
matkuc003 <mnorris.k97@gmail.com>
2019-12-18 13:33:17 +0100

    Character

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Vector3 moveVector;
+    private float speed = 5.0f;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = this.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector = Vector3.zero;
+        if(characterController.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        moveVector.x = Input.GetAxisRaw("Horizontal") *speed;
+        moveVector.y = verticalVelocity;
+        moveVector.z = speed;
+
+        characterController.Move(moveVector * Time.deltaTime);
+    }
+}
