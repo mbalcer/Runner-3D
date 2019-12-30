ffb253f57e059c97beb1b0c7d55551a085ac8602
Szymon1109 <szymonbetlewski@wp.pl>
2019-12-31 00:23:08 +0100

    Added spot light following the player

---
+++ Assets/Scripts/LightManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vector = player.transform.position;
+        vector += new Vector3(0f, 9.0f, -3.0f);
+
+        transform.position = vector;
+    }
+}
